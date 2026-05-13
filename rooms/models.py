11adda8fca9a52240c8f834d8f214fc2a4306eeb
damian <damian.merlak@gmail.com>
2026-05-13 02:44:20 +0200

    air quality index calculation

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from random import randrange
+
+from django.core.cache import cache
 
 class Room(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -7,6 +10,11 @@ class Room(models.Model):
     order = models.PositiveIntegerField(
         default=999,
     )
+    
+    
+    def get_aqi(self):
+        index = cache.get_or_set("room_get_aqi_%s" % self.id, randrange(100), 120)
+        return randrange(100)
 
     def __str__(self):
         return self.name
