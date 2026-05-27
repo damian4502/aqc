96901e660055b4a00921ff1ec4387f4af7d6eb16
damian <damian.merlak@gmail.com>
2026-05-27 02:35:35 +0200

    Added RoomGroup model

--- rooms/models.py
+++ rooms/models.py
@@ -3,6 +3,27 @@ from random import randrange
 
 from django.core.cache import cache
 
+class RoomGroup(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Ime skupine")
+    description = models.TextField(blank=True, verbose_name="Opis")
+    rooms = models.ManyToManyField(
+        'Room',
+        related_name='groups',      
+        blank=True,
+        verbose_name="Prostori v skupini"
+    )
+
+    class Meta:
+        verbose_name = "Skupina prostorov"
+        verbose_name_plural = "Skupine prostorov"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_rooms_list(self):
+        return ", ".join(r.name for r in self.rooms.all())
+        
 class Room(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
